1c2f79119d8ab71684a8ee991fede69fca039a65
bhagyashri7822 <bhagyshriharne@gmail.com>
2026-09-02 17:06:07 +0800

    Run Chrome in headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,15 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver():
-    drv = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    drv = webdriver.Chrome(options=options)
     drv.maximize_window()
+
     yield drv
+
     drv.quit()
